db7f43bf2ae23e863dc25ec8876f2dcd857afcd5
Jared Drueco <drueco03@gmail.com>
2022-07-28 19:28:32 -0600

    done experience base

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="flex items-center flex-col pb-1 border-t-[1px] border-black py-2">
-      <p className="text-xs ">CREATED WITH NEXT.JS AND TAILWINDCSS</p>
+      <p className="text-sm 2xl:text-[16px]">SELF PORTFOLIO 2022</p>
     </div>
   );
 };
