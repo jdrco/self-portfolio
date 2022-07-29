47c1986aa71d5b91a2e8f60d4e1d1ba4883b0a42
Jared Drueco <drueco03@gmail.com>
2022-07-29 16:25:05 -0600

    bug fixing

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="content max-w-sm sm:max-w-xl md:max-w-2xl flex mx-auto justify-between h-screen flex-col min-safe-h-screen">
+    <div className="content max-w-sm sm:max-w-xl md:max-w-2xl flex mx-auto justify-between h-screen flex-col absolute inset-0">
       <Navbar />
       <div>{children}</div>
       <Footer />
