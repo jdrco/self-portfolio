61bc9bb529e4db8196af527761bfb2523f5bfdf6
Jared Drueco <drueco03@gmail.com>
2022-07-29 15:30:06 -0600

    added padding to fix viewport screen issue

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="content max-w-sm sm:max-w-xl md:max-w-2xl flex mx-auto justify-between h-screen flex-col">
+    <div className="content max-w-sm sm:max-w-xl md:max-w-2xl flex mx-auto justify-between h-screen flex-col py-12 sm:py-0">
       <Navbar />
       <div>{children}</div>
       <Footer />
