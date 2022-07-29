96d287611e834582387e4a695b4e2a2e085a8b2f
Jared Drueco <drueco03@gmail.com>
2022-07-29 15:43:56 -0600

    trying to fix viewport on mobile

--- components/Layout.js
+++ components/Layout.js
@@ -3,10 +3,12 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="content max-w-sm sm:max-w-xl md:max-w-2xl flex mx-auto justify-between h-screen flex-col py-12 sm:py-0">
-      <Navbar />
-      <div>{children}</div>
-      <Footer />
+    <div className="absolute inset-0">
+      <div className="content max-w-sm sm:max-w-xl md:max-w-2xl flex mx-auto justify-between h-screen flex-col">
+        <Navbar />
+        <div>{children}</div>
+        <Footer />
+      </div>
     </div>
   );
 };
