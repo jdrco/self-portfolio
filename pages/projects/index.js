62cbac2eb948181a7464b47eaeb5d27f1a8bdeaf
Jared Drueco <jared.drueco@geosonsolutions.com>
2022-07-29 11:02:22 -0600

    fini

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -32,6 +32,8 @@ import {
   SiArduino,
 } from "react-icons/si";
 
+import Link from "next/link";
+
 const Projects = () => {
   return (
     <div className="flex flex-col justify-center items-center h-96 sm:h-[475px]">
@@ -39,7 +41,11 @@ const Projects = () => {
         <div className="w-full h-full">
           <Swiper navigation={true} modules={[Navigation]} className="h-full">
             <SwiperSlide className="flex justify-center items-center">
-              <div className="rounded-[14px] bg-gradient-to-tr from-teal-600 to-blue-800 h-[45%] sm:h-[50%] md:h-[55%] w-[80%] p-3 shadow-md">
+              <a
+                href="https://github.com/jdrco/Courseberta"
+                target="_blank"
+                className="rounded-[14px] bg-gradient-to-tr from-teal-600 to-blue-800 h-[45%] sm:h-[50%] md:h-[55%] w-[80%] p-3 shadow-md"
+              >
                 <div className="h-full w-full flex flex-col">
                   <div className="w-full h-auto flex flex-row justify-between mb-2">
                     <div className="flex flex-col text-white text-sm sm:text-[20px]">
@@ -50,7 +56,7 @@ const Projects = () => {
                     </div>
                     <div className="flex flex-row gap-2 text-white text-3xl sm:text-5xl">
                       <FaJsSquare />
-                      <FaBootstrap className="text-[50px]" />
+                      <FaBootstrap className="text-[32px] sm:text-[50px]" />
                       <FaPython />
                     </div>
                   </div>
@@ -66,10 +72,14 @@ const Projects = () => {
                     Finished in the top 8 of the 2022 HackED Hackathon.
                   </div>
                 </div>
-              </div>
+              </a>
             </SwiperSlide>
             <SwiperSlide className="flex justify-center items-center">
-              <div className="rounded-[14px] bg-gradient-to-tr from-rose-600 to-violet-800 h-[45%] sm:h-[50%] md:h-[55%] w-[80%] p-3 shadow-md">
+              <a
+                href="https://github.com/jdrco/TileDrag"
+                target="_blank"
+                className="rounded-[14px] bg-gradient-to-tr from-yellow-600 to-orange-800 h-[45%] sm:h-[50%] md:h-[55%] w-[80%] p-3 shadow-md"
+              >
                 <div className="h-full w-full flex flex-col">
                   <div className="w-full h-auto flex flex-row justify-between mb-2">
                     <div className="flex flex-col text-white text-sm sm:text-[20px]">
@@ -91,7 +101,69 @@ const Projects = () => {
                     logic resulting in an interactive UX.
                   </div>
                 </div>
-              </div>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="flex justify-center items-center">
+              <a
+                href="https://www.uafc.ca/projects"
+                target="_blank"
+                className="rounded-[14px] bg-gradient-to-tr from-violet-600 to-rose-800 h-[45%] sm:h-[50%] md:h-[55%] w-[80%] p-3 shadow-md"
+              >
+                <div className="h-full w-full flex flex-col">
+                  <div className="w-full h-auto flex flex-row justify-between mb-2">
+                    <div className="flex flex-col text-white text-sm sm:text-[20px]">
+                      <div className="leading-3 sm:leading-6">
+                        Miniature Robotic Arm
+                      </div>
+                      <div className="text-[10px] sm:text-[12px]">
+                        Hardware Design Project
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-2 text-white text-3xl sm:text-5xl">
+                      <FaJsSquare />
+                    </div>
+                  </div>
+                  <div className="bg-black h-full rounded-lg p-3 overflow-auto scroll scrollbar text-xs sm:text-sm text-white">
+                    Assembled a robotic arm with three degrees of movement using
+                    C/C++ programming and an Arduino.
+                    <br />
+                    <br />
+                    Documented the arm’s assembly process and the breadboarding
+                    of servo motors to the Arduino board.
+                  </div>
+                </div>
+              </a>
+            </SwiperSlide>
+            <SwiperSlide className="flex justify-center items-center">
+              <a
+                href="https://github.com/jdrco/Goobers"
+                target="_blank"
+                className="rounded-[14px] bg-gradient-to-tr from-yellow-600 to-blue-800 h-[45%] sm:h-[50%] md:h-[55%] w-[80%] p-3 shadow-md"
+              >
+                <div className="h-full w-full flex flex-col">
+                  <div className="w-full h-auto flex flex-row justify-between mb-2">
+                    <div className="flex flex-col text-white text-sm sm:text-[20px]">
+                      <div className="leading-3 sm:leading-6">
+                        Stock Analyzer
+                      </div>
+                      <div className="text-[10px] sm:text-[12px]">
+                        HackED Beta Hackathon Project
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-2 text-white text-3xl sm:text-5xl">
+                      <FaJsSquare />
+                    </div>
+                  </div>
+                  <div className="bg-black h-full rounded-lg p-3 overflow-auto scroll scrollbar text-xs sm:text-sm text-white">
+                    Developeped a stock analyzer using Python for data analysis
+                    and the Yahoo Finance API to fetch live data.
+                    <br />
+                    <br />
+                    Graphically displayed a stock's moving average using NumPy
+                    and Matplotlib.
+                  </div>
+                </div>
+              </a>
             </SwiperSlide>
           </Swiper>
         </div>
