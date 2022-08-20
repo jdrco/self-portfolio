3b08447815136eb8b6958e84ebf99d0bda1be437
Jared Drueco <drueco03@gmail.com>
2022-08-20 14:44:38 -0600

    New profile photo

--- components/widgets/Readme.js
+++ components/widgets/Readme.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 const Readme = () => {
   return (
     <div className="bg-gradient-to-b from-gray-900 to-gray-600 h-full flex flex-col">
       <div className="bg-blue-200 shadow-lg shadow-slate-200/30 w-[42%] h-[42%] rounded-full ml-7 sm:ml-11 mt-6 sm:mt-10 overflow-hidden relative">
-        <Image src="/profilephoto.jpg" layout="fill" className="object-cover" />
+        <Image src="/photo.jpg" layout="fill" className="object-cover" />
       </div>
       <div className="w-[75%] h-12 ml-7 sm:ml-11 mt-4 sm:mt-7 text-[10px] sm:text-sm md:text-base text-white leading-[11px] sm:leading-[18px] md:leading-[22px]">
         You can click here
