0a1d4f07f6988b97e3ec875065b2547d0462ae32
Jared Drueco <drueco03@gmail.com>
2023-03-06 19:21:41 -0700

    Update readme widget

--- components/widgets/Readme.js
+++ components/widgets/Readme.js
@@ -6,10 +6,8 @@ const Readme = () => {
       <div className="w-[42%] h-[42%] rounded-full ml-7 sm:ml-11 mt-6 sm:mt-10 overflow-hidden relative">
         <Image src="/photo.jpg" layout="fill" className="object-cover" />
       </div>
-      <div className="w-[75%] h-12 ml-7 sm:ml-11 mt-4 sm:mt-7 text-[9px] sm:text-sm md:text-base text-white leading-[11px] sm:leading-[18px] md:leading-[22px]">
-        You can click here
-        <br />
-        to read more about me :)
+      <div className="w-[75%] h-12 ml-8 sm:ml-12 md:ml-[50px] mt-4 sm:mt-7 text-[9px] sm:text-sm md:text-base text-white leading-[11px] sm:leading-[18px] md:leading-[22px]">
+        README
       </div>
     </div>
   );
