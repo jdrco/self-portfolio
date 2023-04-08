75774dc1c5bd67edd3091cc29517d14b6f8a0f61
Jared Drueco <drueco03@gmail.com>
2023-04-07 19:53:23 -0600

    Remove about page. Typewriter readme effect.

--- components/Navbar.js
+++ components/Navbar.js
@@ -25,18 +25,6 @@ const Navbar = () => {
         </div>
         <div className="hidden md:block">
           <div className="ml-10 flex items-baseline space-x-4">
-            <Link href="/about">
-              <a
-                className={
-                  ' px-3 py-2 rounded-md  font-medium' +
-                  (currentRoute === '/about'
-                    ? ' text-red-800'
-                    : ' text-gray-600')
-                }
-              >
-                About
-              </a>
-            </Link>
             <Link href="/experience">
               <a
                 className={
@@ -115,11 +103,6 @@ const Navbar = () => {
 
       <div className={`${isOpen ? 'block' : 'hidden'} md:hidden w-1/2 ml-auto `}>
         <div className="px-2 pt-2 pb-3 sm:px-3">
-          <Link href="/about">
-            <a className="text-right block px-3 py-2 rounded-md text-base font-medium">
-              About
-            </a>
-          </Link>
           <Link href="/experience">
             <a className="text-right block px-3 py-2 rounded-md text-base font-medium">
               Experience
--- components/widgets/Readme.js
+++ components/widgets/Readme.js
@@ -1,19 +1,28 @@
 import Image from 'next/image';
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
-
 import Link from 'next/link';
+import Typewriter from 'typewriter-effect';
 
 const Readme = () => {
   return (
     <div className="flex flex-col">
-      <Image src="/photo.jpg" layout="fill" className="object-cover" />
-      <Link href="/about">
+      <Image src="/photo.jpg" layout="fill" className="object-cover object-left-top scale-125 sm:scale-100 sm:object-center" />
+      <div className="absolute top-6 left-6 text-black text-xl font-light sm:text-2xl">
+        <Typewriter
+          options={{
+            strings: ["hello, i'm jared!", "this is my portfolio"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
+      {/* <Link href="/about">
         <button className="absolute h-12 w-12 bottom-6 right-6 bg-white bg-opacity-20 backdrop-blur-2xl rounded-full">
           <div className="flex items-center justify-center">
             <HiOutlineArrowNarrowRight />
           </div>
         </button>
-      </Link>
+      </Link> */}
     </div>
   );
 };
